0f9c1d9157ad67290ec05fb4b59ca67dbab8d740
omrahina <crave627@gmail.com>
2022-04-21 22:22:06 +0200

    Mediscreen client : add patient information via ui

--- mediscreen/src/main/java/com/mediscreen/mediscreen/controller/MediscreenController.java
+++ mediscreen/src/main/java/com/mediscreen/mediscreen/controller/MediscreenController.java
@@ -3,12 +3,19 @@ package com.mediscreen.mediscreen.controller;
 import com.mediscreen.mediscreen.beans.PatientBean;
 import com.mediscreen.mediscreen.proxies.PatientMicroserviceProxy;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+
 @Controller
+@Slf4j
 public class MediscreenController {
 
     private final PatientMicroserviceProxy patientProxy;
@@ -23,8 +30,32 @@ public class MediscreenController {
             model.addAttribute("patients", patientProxy.patientList());
             model.addAttribute("patient", new PatientBean());
         } catch (FeignException e) {
-            model.addAttribute("error", e.getMessage());
+            error = e.getMessage();
+        }
+        if (error != null){
+            model.addAttribute("error", error);
         }
         return "home";
+    }
+
+    @GetMapping("/patient/add")
+    public String showAddPatientPage(Model model) {
+        model.addAttribute("patient", new PatientBean());
+        return "addPatient";
+    }
+
+    @PostMapping("/patient/add")
+    public String addPatient(@Valid @ModelAttribute("patient") PatientBean patient, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            log.error("Error(s) in the form");
+            return "addPatient";
+        }
+        try {
+            patientProxy.addPatient(patient);
+        } catch (FeignException e) {
+            return "redirect:/patients?error=" + e.getMessage();
+        }
+
+        return "redirect:/patients";
     }
 }
--- mediscreen/src/main/java/com/mediscreen/mediscreen/proxies/PatientMicroserviceProxy.java
+++ mediscreen/src/main/java/com/mediscreen/mediscreen/proxies/PatientMicroserviceProxy.java
@@ -3,6 +3,8 @@ package com.mediscreen.mediscreen.proxies;
 import com.mediscreen.mediscreen.beans.PatientBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -11,5 +13,8 @@ public interface PatientMicroserviceProxy {
 
     @GetMapping("/patients")
     List<PatientBean> patientList();
+
+    @PostMapping("/patient/add")
+    PatientBean addPatient(@RequestBody PatientBean patient);
 
 }
